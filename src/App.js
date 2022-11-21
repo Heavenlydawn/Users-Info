6026ea414e2047ed925c1931d73e9d4e3bfbf876
Heavenlydawn <gabrielheavenlydawn@gmail.com>
2022-11-21 10:52:40 +0100

    Json Modification

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+const UsingFetch = () => {
+  const [users, setUsers] = useState([]);
+
+  const fetchData = () => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setUsers(data);
+      });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col justify-center items-center w-100  p-10">
+    <h1 className= "text-7xl font-bold" >USERS INFORMATION</h1>
+      {users.length > 0 && (
+        <div className="flex gap-20 flex-wrap items-center justify-center my-10">
+          {users.map((user) => (
+            <div className="p-10 bg-red-500 hover:bg-red-300 rounded-lg text-lg">
+              <p><strong>{user.name}</strong> </p>
+              <p>Email: {user.email}</p>
+              <p>Phone: {user.phone}</p>
+              <p> Website: {user.website}</p>
+              <p> Address: {user.address.city}</p>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
-}
+};
 
-export default App;
+export default UsingFetch;
